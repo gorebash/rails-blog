fee39043a8179953b3f8319f4d9327e916318ab0
gorebash <brent.gore@gmail.com>
2023-09-15 19:07:57 -0700

    user edit and show setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
   end
@@ -11,6 +20,21 @@ class UsersController < ApplicationController
       redirect_to articles_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = 'Your account information was successfully updated'
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
